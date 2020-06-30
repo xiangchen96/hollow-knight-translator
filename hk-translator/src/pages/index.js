35cf2fc94ba6749e8eabe4330af8990e43bac82a
Xiang Chen <xiangchenchen96@gmail.com>
2020-07-01 00:24:36 +0100

    Bugfix

--- hk-translator/src/pages/index.js
+++ hk-translator/src/pages/index.js
@@ -15,7 +15,7 @@ function getParameterByName(name, url) {
     let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)')
     let results = regex.exec(url)
 
-    if (!results) return null
+    if (!results) return ''
     if (!results[2]) return ''
 
     return decodeURIComponent(results[2].replace(/\+/g, ' '))
