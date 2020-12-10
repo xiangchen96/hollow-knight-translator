d795e4b4897bb6ca8069b8933cd11b2af8a4d2fe
Xiang Chen <xiangchenchen96@gmail.com>
2020-12-10 23:13:37 +0100

    Update plugin manifest color

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
         name: "hollow-knight-translator",
         short_name: "hk-translator",
         start_url: "/",
-        background_color: "#663399",
-        theme_color: "#663399",
+        background_color: "#e5e7eb",
+        theme_color: "#e5e7eb",
         display: "minimal-ui",
         icon: "src/images/hk-icon.png", // This path is relative to the root of the site.
       },
