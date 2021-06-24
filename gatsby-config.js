5e026974ca59d4561d8dcdea49f3c54a0c4e3522
Xiang Chen Chen <xiangchenchen96@gmail.com>
2021-06-24 20:57:23 +0100

    :arrow_up: Bump dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,20 +12,6 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
-    },
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "hollow-knight-translator",
-        short_name: "hk-translator",
-        start_url: "/",
-        background_color: "#e5e7eb",
-        theme_color: "#e5e7eb",
-        display: "minimal-ui",
-        icon: "src/images/hk-icon.png", // This path is relative to the root of the site.
-      },
-    },
+    }
   ],
 };
